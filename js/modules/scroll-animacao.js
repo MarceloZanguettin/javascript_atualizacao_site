1c66e383a80517600c32f17608904b31cabee6aa
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-03 10:40:39 -0300

    formatado estrutura do script pelo eslint

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,23 +1,20 @@
 export default function initAnimacaoScroll() {
   const section = document.querySelectorAll("[data-anime='scroll']");
-
+  let windowMetade;
+  function animaScroll() {
+    section.forEach(() => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowMetade < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
+      }
+    });
+  }
   if (section.length) {
-    const windowMetade = window.innerHeight * 0.6;
-
-    function animaScroll() {
-      section.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = sectionTop - windowMetade < 0;
-        if (isSectionVisible) {
-          section.classList.add("ativo");
-        } else if (section.classList.contains("ativo")) {
-          section.classList.remove("ativo");
-        }
-      });
-    }
-
+    windowMetade = window.innerHeight * 0.6;
     animaScroll();
-
     window.addEventListener("scroll", animaScroll);
   }
 }
