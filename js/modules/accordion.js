10efb6fd582010846a8529512d5442576ef8d2e1
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-03 10:08:18 -0300

    formatado estrutura do script pelo eslint

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -3,14 +3,13 @@ export default function initAccordion() {
     "[data-anime='accordion'] dt"
   );
   const activeClass = "ativo";
+  function activeAccordion() {
+    this.classList.toggle(activeClass);
+    this.nextElementSibling.classList.toggle(activeClass);
+  }
   if (accordionList.length) {
     accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
-
-    function activeAccordion() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
 
     // função que irá ler o click na pergunta do FAQ e acionar a função activeAccordion
     accordionList.forEach((item) => {
