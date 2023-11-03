4b40358294f7a031e3ff95e9e18b0ecd03896b8e
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-03 15:57:46 -0300

    arrumado os erros no script

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,8 +1,8 @@
 export default function initAnimacaoScroll() {
-  const section = document.querySelectorAll("[data-anime='scroll']");
-  let windowMetade;
+  const section = document.querySelectorAll('[data-anime="scroll"]');
+  const windowMetade = window.innerHeight * 0.6;
   function animaScroll() {
-    section.forEach(() => {
+    section.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
       const isSectionVisible = sectionTop - windowMetade < 0;
       if (isSectionVisible) {
@@ -13,7 +13,6 @@ export default function initAnimacaoScroll() {
     });
   }
   if (section.length) {
-    windowMetade = window.innerHeight * 0.6;
     animaScroll();
     window.addEventListener("scroll", animaScroll);
   }
